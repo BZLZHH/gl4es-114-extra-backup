bcd3f14c2c73688bad433b1a4a192fe70d7207ca
BZLZHH <69153940+BZLZHH@users.noreply.github.com>
2025-03-09 08:09:28 +0800

    directly use desktop glsl

--- src/gl/shader.c
+++ src/gl/shader.c
@@ -175,12 +175,12 @@ void gl4es_glShaderSource(GLuint shader, GLsizei count, const GLchar * const *st
     if (gles_glShaderSource) {
         // adapt shader if needed (i.e. not an es2 context and shader is not #version 100)
         //SHUT_LOGD("Source shader: \n%s", glshader->source);
-        if(glstate->glsl->es2 && !strncmp(glshader->source, "#version 100", 12))
-            glshader->converted = strdup(glshader->source);
-        else{
-            glshader->converted = ConvertShaderConditionally(glshader);
-        }
-
+        //if(glstate->glsl->es2 && !strncmp(glshader->source, "#version 100", 12))
+        //    glshader->converted = strdup(glshader->source);
+        //else{
+        //    glshader->converted = ConvertShaderConditionally(glshader);
+        //}
+        glshader->converted = strdup(glshader->source);
         // send source to GLES2 hardware if any
         gles_glShaderSource(shader, 1, (const GLchar * const*)((glshader->converted)?(&glshader->converted):(&glshader->source)), NULL);
         errorGL();
